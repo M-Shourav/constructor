87ff223af4b7f28b5e09418810e06429fb30718a
M-Shourav <muhammadshourav625@gmail.com>
2024-12-03 23:17:44 +0600

    some changes

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -9,7 +9,7 @@ const Counter = () => {
       <div className="w-full py-20 grid grid-cols-2 md:grid-cols-4 items-center gap-10">
         <div className="flex flex-col gap-2 items-center border border-transparent border-r-gray-400">
           <h1 className="text-xl md:text-5xl font-semibold text-orange-500">
-            <CountUp end={6820} duration={5} />+
+            <CountUp end={6820} duration={8} />+
           </h1>
           <p className="text-base md:text-xl text-gray-500 uppercase">
             clients
@@ -17,7 +17,7 @@ const Counter = () => {
         </div>
         <div className="flex flex-col gap-2 items-center border border-transparent border-r-gray-400">
           <h1 className="text-xl md:text-5xl font-semibold text-orange-500">
-            <CountUp end={4000} duration={5} />+
+            <CountUp end={4000} duration={8} />+
           </h1>
           <p className="text-base md:text-xl text-gray-500 uppercase">
             projects
@@ -25,7 +25,7 @@ const Counter = () => {
         </div>
         <div className="flex flex-col gap-2 items-center border border-transparent border-r-gray-400">
           <h1 className="text-xl md:text-5xl font-semibold text-orange-500">
-            <CountUp end={3568} duration={5} />+
+            <CountUp end={3568} duration={8} />+
           </h1>
           <p className="text-base md:text-xl text-gray-500 uppercase">
             Repairing
@@ -33,7 +33,7 @@ const Counter = () => {
         </div>
         <div className="flex flex-col gap-2 items-center border border-transparent border-r-gray-400">
           <h1 className="text-xl md:text-5xl font-semibold text-orange-500">
-            <CountUp end={1530} duration={5} />+
+            <CountUp end={1530} duration={8} />+
           </h1>
           <p className="text-base md:text-xl text-gray-500 uppercase">awards</p>
         </div>
